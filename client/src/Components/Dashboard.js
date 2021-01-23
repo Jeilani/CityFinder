4a2f98ac44badd9a3a5561d2618d464ba04d3f16
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2021-01-23 11:50:41 -0700

    cleaned up the sidebar for maps

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react"
 import "../CSS/Dashboard.css"
+import BrowseArea from "./DashboardComponents/BrowseArea"
+import HelpAUser from "./DashboardComponents/HelpAUser"
+import Messages from "./DashboardComponents/Messages"
 
 const Dashboard = props => {
     const [dashboardPopUp, setDashboardPopUp] = useState(false)
@@ -12,33 +15,40 @@ const Dashboard = props => {
             <div className="dashboardPopUp">
                 <div className="innerPopUp">
                     <p>Do you want to go to the "{areaClickedIn}" page?</p>
-                    <div className="buttonsContainer"><button>Yes</button><button onClick={()=>{setDashboardPopUp(false); setAreaClickedIn(null)}}>No</button></div>
+                    <div className="buttonsContainer"><button onClick={()=>handlePopUpYes()}>Yes</button><button onClick={()=>{setDashboardPopUp(false); setAreaClickedIn(null)}}>No</button></div>
                 </div>
             </div>)
         } else return null
     }
 
-    const renderDashboard = () => {
-        if (whichDashboardPage === "Dashboard")
-        return (
-            <div className="dashboard">
-                {renderPopUp()}
-                <i onClick={()=>props.setWhichPage("Login")}className="fas logoutButton fa-3x fa-sign-out-alt"></i>
-                <div className="bar" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Messages")}}>
-                    <i className="fas fa-bell"></i>
-                </div>
-                <div className="helpUser" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Help A User")}}>
-                    <h2>Help a user</h2>
-                    <i className="fab fa-3x fa-searchengin"></i>
-                </div>
-                <div className="lookResidence" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Browse Area")}}>
-                    <h2>Browse Area/Residence</h2>
-                    <i className="fas fa-3x fa-key"></i>
-                </div>
-            </div>
-        )
+    const handlePopUpYes = () => {
+        setWhichDashboardPage(areaClickedIn)
+        console.log("whichDashboardPage is ")
+        console.log(areaClickedIn)
     }
-    return renderDashboard()
+
+    if (whichDashboardPage === "Dashboard"){
+    return (
+        <div className="dashboard">
+            {renderPopUp()}
+            <i onClick={()=>props.setWhichPage("Login")}className="far logoutButton fa-3x fa-times-circle"></i>
+            <div className="bar" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Messages")}}>
+                <i className="fas fa-bell"></i>
+            </div>
+            <div className="helpUser" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Help A User")}}>
+                <h2>Help a user</h2>
+                <i className="fab fa-3x fa-searchengin"></i>
+            </div>
+            <div className="lookResidence" onClick={()=>{setDashboardPopUp(true); setAreaClickedIn("Browse Area")}}>
+                <h2>Browse Area/Residence</h2>
+                <i className="fas fa-3x fa-key"></i>
+            </div>
+        </div>
+    )
+    } else if (whichDashboardPage === "Messages") return <Messages/>
+    else if (whichDashboardPage === "Help A User") return <HelpAUser/>
+    else if (whichDashboardPage === "Browse Area") return <BrowseArea setAreaClickedIn={setAreaClickedIn} setDashboardPopUp={setDashboardPopUp}setWhichDashboardPage={setWhichDashboardPage}/>
+
 }
 
 export default Dashboard
--- client/src/Components/DashboardComponents/BrowseArea.js
+++ client/src/Components/DashboardComponents/BrowseArea.js
@@ -1,7 +1,62 @@
-import React from "react"
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import "../../CSS/BrowseArea.css"
 
-const BrowseArea = () => {
-    return (<div> BrowseArea </div>)
+const containerStyle = {
+    left: '30vw',
+  width: '70vw',
+  height: '100vh'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function BrowseArea(props) {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyC2L0nnfFlL_0yZ95DZ4S2vL_nj0EKjghE"
+  })
+  const mapRef = React.useRef()
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    navigator.geolocation.getCurrentPosition(position=>{
+        mapRef.current = map;
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        map.panTo(
+          {lat: position.coords.latitude,
+            lng: position.coords.longitude}
+          )
+          map.setZoom(14)
+          setMap(map)
+        }, (err)=>{console.log(err)});
+    }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      > <div className="searchSideBar">
+          <div onClick={()=>{props.setWhichDashboardPage("Dashboard"); props.setAreaClickedIn(null); props.setDashboardPopUp(false)}} className="returnButton"><i className="fas fa-3x fa-arrow-circle-left"></i></div>
+          <form>
+              <input className="searchInput" type="input" placeholder="Search apartment/city"></input>
+          </form>
+          <p className="clickInformational"><i className="fas fa-info-circle"></i>Click on an area or an apartment to ask questions to residents</p>
+        </div>
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
 
-export default BrowseArea
+export default React.memo(BrowseArea)
