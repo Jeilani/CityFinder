9160963fb66083c99f45fb60e1e57c0ab5bc35a3
Mo Mohieldin <mohieldin@Mohieldins-MacBook-Pro.local>
2021-02-22 16:43:36 -0700

    added alt attribute to image in messages component

--- client/src/Components/DashboardComponents/Messages.js
+++ client/src/Components/DashboardComponents/Messages.js
@@ -7,7 +7,7 @@ const Messages = ({setMessagesShowing}) => {
     const messagesList = mockMessages.map(message => {
         return (
                 <div className="individualMessageContainer">
-                    <img className="msgImage" src={message.userImage}/>
+                    <img className="msgImage" alt = "user" src={message.userImage}/>
                     <div className="msgPreview">{message.messages[0].content.substr(0, 20) + "..."}</div>
                 </div>
             )
