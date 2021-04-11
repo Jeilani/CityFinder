f58e682c9efd9595389e02ac43c84a372a53a88e
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2021-04-11 13:48:48 -0600

    added exit transition to messags component

--- client/src/Components/DashboardComponents/Messages.js
+++ client/src/Components/DashboardComponents/Messages.js
@@ -7,8 +7,16 @@ const Messages = () => {
     const dispatch = useDispatch()
     const messages = useSelector(state=>state.messages)
     const [whichMessageClicked, setWhichMessageClicked] = useState(messages[0])
+    const [hidingActive, setHidingActive] = useState(false)
     const handleMessageClick = (message) => {
         setWhichMessageClicked(message)
+    }
+    const exitMessages = () => {
+        setHidingActive(true)
+        setTimeout(()=>{
+            dispatch(setMessagesHiding())
+        }, 300)
+
     }
     const messagesList = messages.map(message => {
         return (
@@ -24,26 +32,36 @@ const Messages = () => {
             )
 
         })
-    return (
-    <div className="messages">
-        <i onClick={()=>dispatch(setMessagesHiding())} className="fas slidemessagesuparrow fa-2x fa-chevron-up"></i>
-        <div className="messagesSection">
-            <h3>Messages</h3>
-            <div className="messageListContainer">
-                {messagesList}
-            </div>
-        </div>
-        <div className="conversationsSection">
-            <h3>Conversation</h3>
-            <div className="thread">
 
+    if (hidingActive) {
+        return (
+            <div className="hideMessages">
+                <i className="fas fa-envelope"></i>
             </div>
-            <form onEnter={(e)=>{e.preventDefault()}} className="messagingForm">
-                <input placeholder="...type your message here" className="messagingInput" type="text"></input>
-                <button className="sendButton" type="submit"><i className="far fa-paper-plane"></i></button>
-            </form>
-        </div>
-    </div>
-    )
+        )
+    } else {
+        return (
+            <div className="messages">
+                <i onClick={()=>exitMessages()} className="fas slidemessagesuparrow fa-2x fa-chevron-up"></i>
+                <div className="messagesSection">
+                    <h3>Messages</h3>
+                    <div className="messageListContainer">
+                        {messagesList}
+                    </div>
+                </div>
+                <div className="conversationsSection">
+                    <h3>Conversation</h3>
+                    <div className="thread">
+
+                    </div>
+                    <form onEnter={(e)=>{e.preventDefault()}} className="messagingForm">
+                        <input placeholder="...type your message here" className="messagingInput" type="text"></input>
+                        <button className="sendButton" type="submit"><i className="far fa-paper-plane"></i></button>
+                    </form>
+                </div>
+            </div>
+            )
+    }
+
 }
 export default Messages
